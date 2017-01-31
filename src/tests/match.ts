27419ab2d21858ebae4758513fa48342e1e74c35
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-31 13:24:11 +1100

    more tests

--- src/tests/match.ts
+++ src/tests/match.ts
@@ -12,4 +12,44 @@ describe('match', () => {
       params: {}
     });
   });
+
+  it('should match strings ending with :params', () => {
+    const res = match({
+      pattern: '/foo/:bar',
+      path: '/foo/bar'
+    });
+    assert.deepEqual(res, {
+      remainingPath: '',
+      params: {
+        bar: 'bar'
+      }
+    });
+  });
+
+  it('should match strings containing :params', () => {
+    const res = match({
+      pattern: '/foo/:bar/baz',
+      path: '/foo/bar/baz'
+    });
+    assert.deepEqual(res, {
+      remainingPath: '',
+      params: {
+        bar: 'bar'
+      }
+    });
+  });
+
+  it('should match strings containing n :params', () => {
+    const res = match({
+      pattern: '/foo/:bar/:baz',
+      path: '/foo/bar/baz'
+    });
+    assert.deepEqual(res, {
+      remainingPath: '',
+      params: {
+        bar: 'bar',
+        baz: 'baz'
+      }
+    });
+  });
 });
